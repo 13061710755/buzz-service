2529a50b0b3b98d6ae5186652eadf826c0ae4e4f
Jeff Tian <jefftian@hcdlearning.com>
2018-01-26 16:57:26 +0800

    1 dimension k means clustering with fixed center number

--- server/controllers/classScheduleController.js
+++ server/controllers/classScheduleController.js
@@ -1,3 +1,5 @@
+const Scheduling = require("../bll/scheduling");
+
 const promisify = require('../common/promisify');
 const env = process.env.NODE_ENV || "test";
 const config = require("../../knexfile")[env];
@@ -10,6 +12,7 @@ const listSuggested = async ctx => {
             .where('student_class_schedule.start_time', '>=', timeRangeStart)
             .andWhere('student_class_schedule.status', 'booking')
         ;
+        let suggestions = Scheduling.makeGroups(res);
         console.log('res = ', res);
         ctx.body = res;
     } catch (error) {
@@ -17,4 +20,5 @@ const listSuggested = async ctx => {
         ctx.throw(500, error);
     }
 };
+
 module.exports = {listSuggested};
