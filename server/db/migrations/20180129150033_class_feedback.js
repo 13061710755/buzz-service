60caa96cd9e71728656766f7e88c080e2302562f
hank <13061710755@sina.cn>
2018-01-30 16:30:09 +0800

    table feedback primary & foreign

--- server/db/migrations/20180129150033_class_feedback.js
+++ server/db/migrations/20180129150033_class_feedback.js
@@ -1,12 +1,18 @@
 exports.up = function (knex, Promise) {
     return knex.schema.createTable('class_feedback', table => {
-        table.bigIncrements('class_id');
-        table.biginteger('from_user_id');
-        table.biginteger('to_user_id');
+        table.bigInteger('class_id');
+        table.bigInteger('from_user_id');
+        table.bigInteger('to_user_id');
         table.dateTime('feedback_time');
-        table.enum('score', [1, 2, 3, 4, 5]);
+        table.decimal('score');
         table.string('comment');
         table.text('remark');
+
+        table.foreign('class_id').references('classes.class_id');
+        table.foreign('from_user_id').references('users.user_id');
+        table.foreign('to_user_id').references('users.user_id');
+
+        table.primary(['class_id', 'from_user_id', 'to_user_id'])
     })
 };
 
