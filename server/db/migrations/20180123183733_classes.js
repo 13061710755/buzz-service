bd61ea5fa759e7132f74aee45b4e30c6588e8c8f
lbsshuai <15225493153@163.com>
2018-03-06 13:34:53 +0800

    add level in classes

--- server/db/migrations/20180123183733_classes.js
+++ server/db/migrations/20180123183733_classes.js
@@ -2,6 +2,7 @@ exports.up = function (knex, Promise) {
     return knex.schema.createTable('classes', table => {
         table.bigIncrements('class_id');
         table.bigInteger('adviser_id');
+        table.text('level');
         table.dateTime('start_time');
         table.dateTime('end_time');
         table.enum('status', ['opened', 'cancelled', 'ready', 'started', 'ended']);
