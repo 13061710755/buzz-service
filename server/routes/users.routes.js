c0461a28ae50ccb36fed9144646af8447f31e249
Jefåf Tian <jeff.tian@outlook.com>
2018-03-13 13:40:39 +0800

    Log knex config

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -2,7 +2,7 @@ const Router = require("koa-router");
 const router = new Router();
 const usersController = require("../controllers/usersController");
 const BASE_URL = `/api/v1/users`;
-router.get(`${BASE_URL}`, usersController.index);
+router.get(`${BASE_URL}`, usersController.search);
 router.get(`${BASE_URL}/by-facebook/:facebook_id`, usersController.getByFacebookId);
 router.get(`${BASE_URL}/by-wechat`, usersController.getByWechat);
 router.get(`${BASE_URL}/:user_id`, usersController.show);
