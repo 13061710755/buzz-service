48c2f95366d240117ecbdc0e1ebd48fd148eb30a
Jefåf Tian <jeff.tian@outlook.com>
2018-03-15 14:48:20 +0800

    Migrate users table a remark field

--- server/db/migrations/20180118175436_users.js
+++ server/db/migrations/20180118175436_users.js
@@ -1,15 +1,15 @@
 exports.up = function (knex, Promise) {
-    return knex.schema.createTable('users', table => {
-        table.bigIncrements('user_id');
-        table.string('name');
-        table.enum('role', ['s', 'c', 'a']);
-        table.timestamp('created_at').defaultTo(knex.fn.now());
+    return knex.schema.hasTable('users').then(exists => {
+        if (!exists) {
+            return knex.schema.createTable('users', table => {
+                table.bigIncrements('user_id');
+                table.string('name');
+                table.enum('role', ['s', 'c', 'a']);
+                table.timestamp('created_at').defaultTo(knex.fn.now());
 
-        // table.primary('user_id');
-    }).then(() => {
-        return knex.schema.table('users', table => {
-            table.text('remark');
-        })
+                // table.primary('user_id');
+            })
+        }
     })
 };
 
---
+++ server/db/migrations/20180315144519_users.js
@@ -0,0 +1,11 @@
+exports.up = function (knex, Promise) {
+    return knex.schema.table('users', table => {
+        table.text('remark');
+    })
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.table('users', table => {
+        table.dropColumn('remark');
+    })
+};
