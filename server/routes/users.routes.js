2440f870484d5994db3a722de286a421dca06ffe
Hsiaoyi Hsu <xreamxu@gmail.com>
2018-03-19 15:29:56 +0800

    [+] getWechatJsConfig Router

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,13 +1,14 @@
-const Router = require("koa-router");
-const router = new Router();
-const usersController = require("../controllers/usersController");
-const BASE_URL = `/api/v1/users`;
-router.get(`${BASE_URL}`, usersController.search);
-router.get(`${BASE_URL}/by-facebook/:facebook_id`, usersController.getByFacebookId);
-router.get(`${BASE_URL}/by-wechat`, usersController.getByWechat);
-router.get(`${BASE_URL}/:user_id`, usersController.show);
-router.post(`${BASE_URL}`, usersController.create);
-router.put(`${BASE_URL}/sign-in`, usersController.signIn);
-router.put(`${BASE_URL}/:user_id`, usersController.update);
-router.del(`${BASE_URL}/:user_id`, usersController.delete);
-module.exports = router;
+const Router = require('koa-router')
+const router = new Router()
+const usersController = require('../controllers/usersController')
+const BASE_URL = '/api/v1/users'
+router.get(`${BASE_URL}`, usersController.search)
+router.get(`${BASE_URL}/by-facebook/:facebook_id`, usersController.getByFacebookId)
+router.get(`${BASE_URL}/by-wechat`, usersController.getByWechat)
+router.get(`${BASE_URL}/:user_id`, usersController.show)
+router.post(`${BASE_URL}`, usersController.create)
+router.put(`${BASE_URL}/sign-in`, usersController.signIn)
+router.put(`${BASE_URL}/:user_id`, usersController.update)
+router.del(`${BASE_URL}/:user_id`, usersController.delete)
+router.post(`${BASE_URL}/getWechatJsConfig`, usersController.getWechatJsConfig)
+module.exports = router
