2d1b72ca0d903f4e0a8616a8e34b1d1fd0cbde10
Hsiaoyi Hsu <xreamxu@gmail.com>
2018-03-20 17:52:38 +0800

    [+] qiniu avvod

--- server/common/wechat.js
+++ server/common/wechat.js
@@ -1,18 +1,30 @@
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
 const Client = require('co-wechat-oauth')
 const API = require('co-wechat-api')
 
 const appId = process.env.buzz_wechat_appid
 const appSecret = process.env.buzz_wechat_secret
 
-const api = new API(appId, appSecret)
-
+// TODO: redis
+const apiFile = path.join(os.tmpdir(), `wechat_api_${appId}.txt`)
+const api = new API(
+  appId, appSecret,
+  async () => {
+    try {
+      JSON.parse(fs.readFileSync(apiFile, 'utf8'))
+    } catch (e) {}
+  },
+  async token => fs.writeFileSync(apiFile, JSON.stringify(token))
+)
 const client = new Client(appId, appSecret)
 
 module.exports = {
   async getJsConfig(arg) {
     return await api.getJsConfig(arg)
   },
-  async getMedia({ id }) {
-    return await api.getMedia(id)
+  async getMedia({ serverId }) {
+    return await api.getMedia(serverId)
   },
 }
