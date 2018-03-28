f3c9da10dfebb7c43d0d85fe091dd4bf34094556
Jefåf Tian <jeff.tian@outlook.com>
2018-03-28 14:22:59 +0800

    Add test port

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-npm install &&
+git reset master --hard &&
+git pull &&
+npm test &&
+npm install --dev &&
 knex migrate:latest --env production &&
 pm2 startOrGracefulReload ecosystem.config.js &&
 pm2 save
